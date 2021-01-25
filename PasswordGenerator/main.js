a57d95cc62a92a2fb277b37b0773ba97fcc638bb
gitSnake <GitSnake@users.noreply.github.com>
2021-01-25 14:39:56 -0500

    Added symbols function

--- PasswordGenerator/main.js
+++ PasswordGenerator/main.js
@@ -12,4 +12,9 @@ function getRandomNumber(){
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-console.log(Math.floor(Math.random() * 26) + 97);
+function getRandomSymbol(){
+  const symbols = '!@#$%^&*(){}[]=<>?/,.'
+  return symbols[];
+}
+
+console.log(getRandomNumber());
