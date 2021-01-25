bbc6474b1e7e4f0ce1ee28b3f08e363480464cc8
gitSnake <GitSnake@users.noreply.github.com>
2021-01-25 14:55:31 -0500

    Added (;)

--- PasswordGenerator/main.js
+++ PasswordGenerator/main.js
@@ -1,12 +1,12 @@
 // DOM elements
 const resultEl = document.getElementById('result');
 const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('result');
-const resultEl = document.getElementById('result');
-const resultEl = document.getElementById('result');
-const resultEl = document.getElementById('result');
-const resultEl = document.getElementById('result');
-const resultEl = document.getElementById('result');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numbesrEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
+const generateEl = document.getElementById('generate');
+const clipboardEl = document.getElementById('clipboard');
 
 
 const randomFunc = {
@@ -14,7 +14,7 @@ const randomFunc = {
   upper: getRandomUpper,
   number: getRandomNumber,
   symbol: getRandomSymbol
-}
+};
 
 
 
