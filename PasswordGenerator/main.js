0f38fb238094a574fad93f52e9b5665a9d4e9d9a
gitSnake <GitSnake@users.noreply.github.com>
2021-01-25 11:31:28 -0500

    Added getRandomUpper function

--- PasswordGenerator/main.js
+++ PasswordGenerator/main.js
@@ -4,4 +4,8 @@ function getRandomLower(){
   return String.fromCharCode(97)
 }
 
+function getRandomUpper(){
+  return String.fromCharCode(97)
+}
+
 console.log(Math.floor(Math.random() * 26) + 97);
