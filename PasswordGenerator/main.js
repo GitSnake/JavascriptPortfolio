be634121206cd0c8007dcce58ea848bed47e1a3e
gitSnake <GitSnake@users.noreply.github.com>
2021-01-25 11:35:17 -0500

    Changed upper number to 65

--- PasswordGenerator/main.js
+++ PasswordGenerator/main.js
@@ -5,7 +5,7 @@ function getRandomLower(){
 }
 
 function getRandomUpper(){
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 console.log(Math.floor(Math.random() * 26) + 97);
