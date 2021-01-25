3622acdac4f509398f676e5cd94eb1e6a1a2950f
gitSnake <GitSnake@users.noreply.github.com>
2021-01-25 11:33:48 -0500

    Added numer code to functions

--- PasswordGenerator/main.js
+++ PasswordGenerator/main.js
@@ -1,11 +1,11 @@
 // Generator Functions
 
 function getRandomLower(){
-  return String.fromCharCode(97)
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper(){
-  return String.fromCharCode(97)
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 console.log(Math.floor(Math.random() * 26) + 97);
